7a71b021013b6686ad01b2446feafb2b1bbe1723
Tapasya <tapkun@Tapasya.lan>
2018-07-29 15:44:53 +0530

    Added Testing file and .gitignore

---
+++ testing.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+class Testing{
+
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter anyhting!!!!");
+		System.out.println(in.nextLine());
+	}
+
+}
